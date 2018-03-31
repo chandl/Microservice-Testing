421e7228eb2cf3ac7e819070d05cf115f0eba8bc
Chandler Severson <me@cseverson.com>
2018-03-30 23:45:21 -0700

    Continue working on WebPlayerService

--- src/main/java/com/cseverson/cryptomals/services/web/player/WebPlayerServer.java
+++ src/main/java/com/cseverson/cryptomals/services/web/player/WebPlayerServer.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
